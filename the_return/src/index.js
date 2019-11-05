8cba498fef767f8a737b5cfd7b88246bad16cf17
CalWal <callumtrwallace@gmail.com>
2019-11-05 18:46:21 +1000

    starting from scratch

--- the_return/src/index.js
+++ the_return/src/index.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+// Import the React and ReactDOM libraries
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//Create a react component
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const App = function() {
+  return <div>Hi there!</div>;
+};
+
+//T Take the react component and show it on the browser
+ReactDOM.render(<App />, document.querySelector("#root"));
